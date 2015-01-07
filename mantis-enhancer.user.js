5d26fc84717d3d92f16d8e6657f230dc3aa4f48b
flopp <fpigorsch@gmail.com>
2015-01-07 09:41:03 +0100

    Update mantis-enhancer.user.js

--- mantis-enhancer.user.js
+++ mantis-enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mantis Enhancer
 // @namespace    http://flopp.net/
-// @version      0.2.8
+// @version      0.2.9
 // @description  Several enhancements for Mantis BT
 // @author       Florian Pigorsch
 // @downloadURL  https://raw.githubusercontent.com/flopp/mantis-enhancer-user-script/master/mantis-enhancer.user.js
